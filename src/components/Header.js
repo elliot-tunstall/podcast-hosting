31180383b674610e4ecf7c1e3763e5ab4a837fa8
elliot-tunstall <elliot.tunstall@gmail.com>
2024-09-23 22:47:14 +0100

    added animation to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
 import { WEBSITE_NAME } from '../data/websiteConfig';
+import microphone from '../images/microphoneAnimation.webm'
 
 function Header() {
+  const videoRef = useRef(null); // Create a ref for the video element
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.playbackRate = 1; // Set playback speed to half
+    }
+  }, []);
+
   return (
     <header className="header">
       <div className="container">
         <div className="header-content">
-          <Link to="/" className="logo">{ WEBSITE_NAME }</Link>
+          <div className='center-element'>
+            <video ref={videoRef} width="60" height="60" autoPlay muted
+            onMouseEnter={() => videoRef.current.play()} // Play on hover
+            >
+              <source src={microphone} type="video/webm" />
+              Your browser does not support the video tag.
+            </video>
+            <Link to="/" className="logo">{ WEBSITE_NAME }</Link>
+          </div>
           <nav className="nav-menu">
             <ul>
               <li><Link to="/">Home</Link></li>
